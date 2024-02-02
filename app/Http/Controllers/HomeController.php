1e6d5bb1f349c8a1dbfbcad75b7c3a72e768448b
nklido <nikos.klido@gmail.com>
2024-02-02 18:18:44 +0200

    HomeController show home screen / filters results based on postal code

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PostalCode;
+use App\Models\StreetNumber;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function __invoke(Request $request){
+
+        $numbers = [];
+
+        if($request->postal_code){
+            $postal = $request->postal_code;
+            $numbers = StreetNumber::with('street.postalCode')
+                ->whereHas('street.postalCode',function($q) use ($postal){
+                    $q->where('code',$postal);
+            })->get();
+        }
+
+        $postalCodes = PostalCode::athens()
+            ->orderBy('ftth_count','desc')
+            ->get();
+
+        return view('welcome',compact('numbers','postalCodes'));
+    }
+}
