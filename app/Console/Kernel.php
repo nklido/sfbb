3c30464ae78a5244dba680dde4dd5a4e426bdd76
nklido <nikos.klido@gmail.com>
2024-02-02 18:13:57 +0200

    Remove laravel's example kernel commands

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+
     }
 
     /**
@@ -26,7 +26,5 @@ class Kernel extends ConsoleKernel
     protected function commands()
     {
         $this->load(__DIR__.'/Commands');
-
-        require base_path('routes/console.php');
     }
 }
