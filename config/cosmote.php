55b3495e3ff705fa809e9c0aee6ca295e216d276
nklido <nikos.klido@gmail.com>
2025-06-29 14:07:51 +0300

    Add missing cosmote configuration file

---
+++ config/cosmote.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'availability_url' => env('COSMOTE_AVAILABILITY_URL', 'https://www.cosmote.gr/eshop/jsp/ajax/avdslavailabilityAjaxV2.jsp'),
+    'area_url' => env('COSMOTE_AREA_URL', 'https://www.cosmote.gr/eshop/global/gadgets/populateAddressDetailsV3.jsp'),
+];
