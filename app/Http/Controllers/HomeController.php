883be8e7a2410a54ea6bc0026f8ef0477ce6d56a
nklido <nikos.klido@gmail.com>
2025-06-29 11:03:34 +0300

    Home page: Order street numbers by natural order

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,22 +8,29 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function __invoke(Request $request){
+    public function __invoke(Request $request)
+    {
 
         $numbers = [];
 
-        if($request->postal_code){
+        if ($request->postal_code) {
             $postal = $request->postal_code;
             $numbers = StreetNumber::with('street.postalCode')
-                ->whereHas('street.postalCode',function($q) use ($postal){
-                    $q->where('code',$postal);
-            })->get();
+                ->whereHas('street.postalCode', function ($q) use ($postal) {
+                    $q->where('code', $postal);
+                })->get()
+                ->sortBy([
+                    fn($a, $b) => strnatcmp($a->street->name, $b->street->name),
+                    fn($a, $b) => strnatcmp($a->number, $b->number),
+                ])
+                ->values();
+
         }
 
         $postalCodes = PostalCode::athens()
-            ->orderBy('ftth_count','desc')
+            ->orderBy('ftth_count', 'desc')
             ->get();
 
-        return view('home',compact('numbers','postalCodes'));
+        return view('home', compact('numbers', 'postalCodes'));
     }
 }
